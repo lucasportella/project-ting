3c41d7de3c50eb56b6a969b7d5d7171dbcd36e62
lucas <lucasportellaagu@gmail.com>
2022-01-17 13:21:33 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,4 +2,15 @@ import sys
 
 
 def txt_importer(path_file):
-    pass
+    file_extension = path_file.split(".")[-1]
+    if file_extension != "txt":
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, mode="rt") as file:
+            content = file.read()
+            return content.split("\n")
+    except:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
+txt_importer("statics/arquivo_test.txt")
